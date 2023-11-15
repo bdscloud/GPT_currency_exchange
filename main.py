8b9813b57c04212c5340a54ee350044b836bf8e2
Brice de Soras <b.desoras@gmail.com>
2023-11-15 11:44:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 pip install requests
+pip install Flask
+from flask import Flask, request, jsonify
 import requests
 
+app = Flask(__name__)
 
-# Main Program
 api_key = "9f9d3f9dabaf4f97a6558cbad4ab1088"
-
 
 def convert_currency(amount, from_currency, to_currency, api_key):
     url = f"https://api.exchangeratesapi.io/v1/convert?access_key={api_key}&from={from_currency}&to={to_currency}&amount={amount}"
     
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
-
+        response.raise_for_status()
         data = response.json()
         if data["success"]:
-            return data["result"]
+            return data
         else:
-            return "Error: API request unsuccessful"
+            return {"error": "API request unsuccessful"}
     except requests.exceptions.RequestException as e:
-        return f"Error: {e}"
+        return {"error": str(e)}
 
-# Example usage
-api_key = "your_api_key"
-amount = 100
-from_currency = "USD"
-to_currency = "EUR"
-result = convert_currency(amount, from_currency, to_currency, api_key)
-print(f"{amount} {from_currency} is equal to {result} {to_currency}")
+@app.route('/convert', methods=['GET'])
+def handle_convert():
+    amount = request.args.get('amount', type=float)
+    from_currency = request.args.get('from')
+    to_currency = request.args.get('to')
+
+    if not all([amount, from_currency, to_currency]):
+        return jsonify({"error": "Missing required parameters"}), 400
+
+    result = convert_currency(amount, from_currency, to_currency, api_key)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
